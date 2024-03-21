4d6d6df0c7951a32b260ad126a8c3d48abdd2727
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-03-21 18:04:54 -0300

    Ajuste de variáveis

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function calcularPert() {
     const inputPessimista = document.getElementById('pessimista');
     const spanResultado = document.getElementById('resultado');
 
-    let valueProvavel = inputProvavel.value ? Number(inputProvavel.value) : 0;
-    let valueOtimista = inputOtimista.value ? Number(inputOtimista.value) : 0;
-    let valuePessimista = inputPessimista.value ? Number(inputPessimista.value) : 0;
+    const valueProvavel = inputProvavel.value ? Number(inputProvavel.value) : 0;
+    const valueOtimista = inputOtimista.value ? Number(inputOtimista.value) : 0;
+    const valuePessimista = inputPessimista.value ? Number(inputPessimista.value) : 0;
 
-    let pert = (valueOtimista + 4 * valueProvavel + valuePessimista) / 6;
+    const pert = (valueOtimista + 4 * valueProvavel + valuePessimista) / 6;
 
     if (pert > 0) {
         spanResultado.textContent = `PERT: ${pert.toFixed(2)}h / Provável (${valueProvavel}h) / Otimista (${valueOtimista}h) / Pessimista (${valuePessimista}h)`;
